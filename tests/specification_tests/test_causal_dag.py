85aec077f797702479caf13d618d8a11fcd6027b
AndrewC19 <agclark2@sheffield.ac.uk>
2021-11-15 16:54:16 +0000

    Updated requirements.txt and added causal DAG tests

--- tests/specification_tests/test_causal_dag.py
+++ tests/specification_tests/test_causal_dag.py
@@ -1,5 +1,6 @@
 import unittest
 import os
+import networkx as nx
 from causal_testing.specification.causal_specification import CausalDAG
 
 
@@ -12,9 +13,20 @@ class TestCausalDAG(unittest.TestCase):
         f.write(dag_dot)
         f.close()
 
-    def test_something(self):
+    def test_valid_causal_dag(self):
         causal_dag = CausalDAG(self.dag_dot_path)
-        print(causal_dag)
+        assert list(causal_dag.graph.nodes) == ['A', 'B', 'C', 'D'] and list(causal_dag.graph.edges) == [('A', 'B'),
+                                                                                                         ('B', 'C'),
+                                                                                                         ('D', 'A'),
+                                                                                                         ('D', 'C')]
+
+    def test_invalid_causal_dag(self):
+        causal_dag = CausalDAG(self.dag_dot_path)
+        self.assertRaises(nx.HasACycle, causal_dag.add_edge, 'C', 'A')
+
+    def test_empty_casual_dag(self):
+        causal_dag = CausalDAG()
+        assert list(causal_dag.graph.nodes) == [] and list(causal_dag.graph.edges) == []
 
     def tearDown(self) -> None:
         os.remove('temp/dag.dot')
