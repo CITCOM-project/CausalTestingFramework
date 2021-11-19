4c49f57b0e2e9c23aaf820097a93ec2fc2e2eb72
AndrewC19 <agclark2@sheffield.ac.uk>
2021-11-19 16:56:41 +0000

    Refactored casual specification to meet style guidelines

--- causal_testing/specification/causal_specification.py
+++ causal_testing/specification/causal_specification.py
@@ -1,6 +1,9 @@
 from abc import ABC
-from causal_testing.specification.constraint import NormalDistribution
+from causal_testing.specification.constraint import Constraint, NormalDistribution
+from typing import Union
 import networkx as nx
+
+Node = Union[str, int]  # Node type hint: A node is a string or an int
 
 
 class CausalDAG(nx.DiGraph):
@@ -10,7 +13,7 @@ class CausalDAG(nx.DiGraph):
     ensures it is acyclic. A CausalDAG must be specified as a dot file.
     """
 
-    def __init__(self, dot_path=None, **attr):
+    def __init__(self, dot_path: str = None, **attr):
         super().__init__(**attr)
         if dot_path:
             self.graph = nx.DiGraph(nx.drawing.nx_agraph.read_dot(dot_path))
@@ -20,7 +23,7 @@ class CausalDAG(nx.DiGraph):
         if not self.is_acyclic():
             raise nx.HasACycle("Invalid Causal DAG: contains a cycle.")
 
-    def add_edge(self, u_of_edge, v_of_edge, **attr):
+    def add_edge(self, u_of_edge: Node, v_of_edge: Node, **attr):
         """
         Add an edge to the causal DAG. Overrides the default networkx method to prevent users from adding a cycle.
         :param u_of_edge: From node
@@ -31,19 +34,59 @@ class CausalDAG(nx.DiGraph):
         if not self.is_acyclic():
             raise nx.HasACycle("Invalid Causal DAG: contains a cycle.")
 
-    def is_acyclic(self):
+    def is_acyclic(self) -> bool:
         """
         Checks if the graph is acyclic.
         :return: True if acyclic, False otherwise.
         """
         return not list(nx.simple_cycles(self.graph))
 
+    def get_minimal_adjustment_set(self, treatments: [str], outcomes: [str]) -> [str]:
+        """
+        Get the smallest possible set of variables that blocks all back-door paths between the all pairs of treatments
+        and outcomes.
+        :param treatments: A list of strings representing treatments.
+        :param outcomes: A list of strings representing outcomes.
+        :return: A list of strings representing the minimal adjustment set.
+        """
+        backdoor_graph = self.get_proper_backdoor_graph(treatments, outcomes)
+        return backdoor_graph.minimal_d_separator(treatments, outcomes)
+
+    def get_proper_backdoor_graph(self, treatments: [str], outcomes: [str]) -> 'CausalDAG':
+        """
+        Convert the causal DAG to a proper back-door graph. A proper back-door graph of a causal DAG is obtained by
+        removing the first edge of every proper causal path from X to Y. A proper causal path from X to Y is a path
+        of directed edges that starts from X and ends in Y.
+
+        Reference: (Separators and adjustment sets in causal graphs: Complete criteria and an algorithmic framework,
+        Zander et al.,  2019, Definition 3, p.15)
+
+        :param treatments: A list of treatment variables.
+        :param outcomes: A list of outcomes.
+        :return: A CausalDAG corresponding to the proper back-door graph.
+        """
+        pass
+
+    def minimal_d_separator(self, treatments: [str], outcomes: [str]) -> [str]:
+        """
+        Get the smallest set of variables which d-separates treatments from outcomes. In other words, a set of variables
+        from which the removal of any subset of variables would no d-connect the treatments and outcomes (i.e. no longer
+        block all back-door paths).
+
+        :param treatments: A list of treatment variables.
+        :param outcomes: A list of outcomes.
+        :return: A list of variables representing the smallest set of variables that d-separates the treatments from the
+        outcomes.
+        """
+        pass
+
     def __str__(self):
         return f'Nodes: {self.graph.nodes}\nEdges: {self.graph.edges}'
 
 
 class Scenario(dict):
-    """ Given a system with X distinct inputs, a scenario is a series of constraints placed over a subset of these
+    """
+    Given a system with X distinct inputs, a scenario is a series of constraints placed over a subset of these
     inputs that characterises some use-case of the system-under-test.
     """
 
@@ -51,10 +94,10 @@ class Scenario(dict):
         super().__init__(*args, **kwargs)
         self.__dict__ = self
 
-    def add_constraint(self, input_variable, constraint):
+    def add_constraint(self, input_variable: str, constraint: Constraint):
         self[input_variable] = constraint
 
-    def add_constraints(self, constraints_dict):
+    def add_constraints(self, constraints_dict: dict):
         self.update(constraints_dict)
 
 
