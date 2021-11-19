742a40baae68942c8b3f242699ae16f8cd14fe7d
AndrewC19 <agclark2@sheffield.ac.uk>
2021-11-19 17:03:25 +0000

    Added documentation and type hints to constraints

--- causal_testing/specification/constraint.py
+++ causal_testing/specification/constraint.py
@@ -1,20 +1,36 @@
 from abc import ABC, abstractmethod
+from typing import Union
 import numpy as np
 
-class Constraint(ABC):
 
+class Constraint(ABC):
+    """
+    A constraint is a restriction that can be assigned to a subset of inputs that characterises a particular usage
+    scenario for the system-under-test.
+    """
     @abstractmethod
-    def sample_value(self):
-        ...
+    def sample_value(self) -> Union[int, float, str]:
+        """
+        Sample a value from the specified constraint (absolute value or some distribution).
+        :return: A value sampled at random from the applied constraint (absolute value or some distribution).
+        """
+        pass
 
 
 class NormalDistribution(Constraint):
+    """
+    A normal distribution which can be used to constrain the values of inputs in a Scenario.
+    """
 
-    def __init__(self, mean, var):
+    def __init__(self, mean: Union[int, float], var: Union[int, float]):
         self.mean = mean
         self.var = var
 
-    def sample_value(self):
+    def sample_value(self) -> float:
+        """
+        Sample a value at random from the normal distribution.
+        :return: A randomly sampled value from the normal distribution.
+        """
         return np.random.normal(self.mean, self.var)
 
     def __str__(self):
@@ -26,10 +42,14 @@ class NormalDistribution(Constraint):
 
 class AbsoluteValue(Constraint):
 
-    def __init__(self, value):
+    def __init__(self, value: Union[int, float, str]):
         self.value = value
 
-    def sample_value(self):
+    def sample_value(self) -> Union[int, float, str]:
+        """
+        Sample the value of the absolute value constraint.
+        :return: The value of the absolute value constraint.
+        """
         return self.value
 
     def __str__(self):
@@ -40,15 +60,19 @@ class AbsoluteValue(Constraint):
 
 
 class UniformDistribution(Constraint):
-    def __init__(self, min, max):
-        self.min = min
-        self.max = max
+    def __init__(self, lower, upper):
+        self.lower = lower
+        self.upper = upper
 
-    def sample_value(self):
-        return np.random.uniform(min, max)
+    def sample_value(self) -> float:
+        """
+        Sample a value from the uniform distribution.
+        :return: A float sampled at random from the uniform distribution.
+        """
+        return np.random.uniform(self.lower, self.upper)
 
     def __str__(self):
-        return f'U[{self.min}, {self.max}]'
+        return f'U[{self.lower}, {self.upper}]'
 
     def __repr__(self):
-        return f'~ U[{self.min}, {self.max}]'
+        return f'~ U[{self.lower}, {self.upper}]'
