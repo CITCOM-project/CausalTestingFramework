a03b750714b22551facf3f43568501d4c7db1a77
Andrew Clark <agclark2@sheffield.ac.uk>
2021-11-19 13:33:02 +0000

    Add requirements to setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,15 @@
 from setuptools import setup
+
+requirements = [
+    numpy~=1.21.4,
+    pandas~=1.3.4,
+    setuptools~=58.5.3,
+    networkx~=2.6.3,
+    pygraphviz~=1.7,
+    dowhy~=0.6,
+    rpy2~=3.4.5
+]
+
 setup(
     name='causaltest',
     version='0.0.1',
