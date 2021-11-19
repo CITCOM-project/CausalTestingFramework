e1353be53a9935f91c5d3e955f2f92c48bb844ef
AndrewC19 <agclark2@sheffield.ac.uk>
2021-11-19 16:02:15 +0000

    Refactored data collectors to meet style guidelines

--- causal_testing/data_collection/data_collector.py
+++ causal_testing/data_collection/data_collector.py
@@ -1,25 +1,26 @@
 from abc import ABC, abstractmethod
 from causal_testing.specification.causal_specification import Scenario
-from causal_testing.specification.constraint import UniformDistribution, AbsoluteValue
+from causal_testing.specification.constraint import Constraint, UniformDistribution, AbsoluteValue
+from typing import Union
 import pandas as pd
 
 
 class DataCollector(ABC):
 
     @abstractmethod
-    def collect_data(self, **kwargs):
+    def collect_data(self, **kwargs) -> pd.DataFrame:
         """
         Populate the dataframe with execution data.
-        :return:
+        :return df: A pandas dataframe containing execution data for the system-under-test.
         """
-        ...
+        pass
 
 
 class ExperimentalDataCollector(DataCollector):
     """
     Users should implement these methods to collect data from their system directly.
     """
-    def __init__(self, control_input_configuration, treatment_input_configuration, n_repeats=1):
+    def __init__(self, control_input_configuration: dict, treatment_input_configuration: dict, n_repeats: int = 1):
         super().__init__()
         self.control_input_configuration = control_input_configuration
         self.treatment_input_configuration = treatment_input_configuration
@@ -27,10 +28,23 @@ class ExperimentalDataCollector(DataCollector):
 
     @abstractmethod
     def collect_data(self, **kwargs) -> pd.DataFrame:
-        pass
+        """
+        Populate the dataframe with execution data.
+        :return df: A pandas dataframe containing execution data for the system-under-test in both control and treatment
+        executions.
+        """
+        control_df = self.run_system_with_input_configuration(self.control_input_configuration)
+        treatment_df = self.run_system_with_input_configuration(self.treatment_input_configuration)
+        return pd.concat([control_df, treatment_df], keys=['control', 'treatment'])
 
     @abstractmethod
-    def run_system_with_input_configuration(self, input_configuration):
+    def run_system_with_input_configuration(self, input_configuration: dict) -> pd.DataFrame:
+        """
+        Run the system with a given input configuration and return the resulting execution data.
+        :param input_configuration: A dictionary which maps a subset of inputs to values.
+        :return df: A pandas dataframe containing execution data obtained by executing the system-under-test with the
+        specified input configuration.
+        """
         pass
 
 
@@ -40,33 +54,46 @@ class ObservationalDataCollector(DataCollector):
         super().__init__()
         self.scenario = scenario
 
-    def collect_data(self, csv_path):
-        df = pd.read_csv(csv_path)
-        df = self.filter_valid_data(df)
-        return df
-
-    def filter_valid_data(self, df):
+    def collect_data(self, csv_path: str) -> pd.DataFrame:
         """
-        Check if observational df is valid for a given scenario.
+        Read a csv containing execution data for the system-under-test into a pandas dataframe and filter to remove any
+        data which does is invalid for the scenario-under-test. Data is invalid if it does not meet the constraints
+        outlined in the scenario-under-test (Scenario).
+        :param csv_path: Path to the csv containing execution data.
+        :return scenario_execution_data_df: A pandas dataframe containing execution data that is valid for the
+        scenario-under-test.
+        """
+        execution_data_df = pd.read_csv(csv_path)
+        scenario_execution_data_df = self.filter_valid_data(execution_data_df)
+        return scenario_execution_data_df
+
+    def filter_valid_data(self, execution_data_df: pd.DataFrame) -> pd.DataFrame:
+        """
+        Check is execution data is valid for the scenario-under-test. Data is invalid if it does not meet the
+        constraints imposed in the scenario-under-test (Scenario).
+        :param execution_data_df: A pandas dataframe containing execution data from the system-under-test.
+        :return:
         """
         variables = set(self.scenario.keys())
 
         # Check positivity
-        if not variables.issubset(df.columns):
-            missing_variables = variables - set(df.columns)
+        if not variables.issubset(execution_data_df.columns):
+            missing_variables = variables - set(execution_data_df.columns)
             raise IndexError(f'Positivity violation: missing data for variables {missing_variables}.')
 
         # For each variable in the scenario, remove rows where a constraint is violated
+        scenario_execution_data_df = execution_data_df.copy()
         for variable, constraint in self.scenario.items():
-            df = df[df[variable].map(lambda x: value_meets_constraint(x, constraint))]
+            scenario_execution_data_df = execution_data_df[
+                execution_data_df[variable].map(lambda x: value_meets_constraint(x, constraint))]
 
-        return df
+        return scenario_execution_data_df
 
 
-def value_meets_constraint(value, constraint):
+def value_meets_constraint(value: Union[int, float], constraint: Constraint) -> bool:
     """
-    Check that value-specific constraints are met.
-    :param value: The value to check.
+    Check that numerical value-specific constraints are met.
+    :param value: The numerical value to check.
     :param constraint: The constraint to check the value against.
     :return: True or False depending on whether the value satisfies the constraint.
     """
