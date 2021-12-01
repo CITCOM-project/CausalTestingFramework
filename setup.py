3c65e2de37077649ccb65d7f33c374ff5f22130a
bobturneruk <r.d.turner@sheffield.ac.uk>
2021-12-01 11:26:39 +0000

    specify requirements

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ requirements = [
     'pandas~=1.3.4',
     'setuptools~=58.5.3',
     'networkx~=2.6.3',
-    'pygraphviz~=1.7',
-    'pytest~=6.2.5'
+    'pygraphviz~=1.7'
 ]
 
 setup(
     name='causal_testing_framework',
     version='0.0.1',
+    install_requires=requirements,
     packages=find_packages()
 )
