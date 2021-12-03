a700d9b28c150c6d30ec1a4a55dca48cc790268e
AndrewC19 <agclark2@sheffield.ac.uk>
2021-12-03 10:06:20 +0000

    Added econml and statsmodel to requirements, downgraded numpy to 1.20

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from setuptools import setup, find_packages
 
 requirements = [
-    'numpy~=1.21.4',
+    'numpy~=1.20',
     'pandas~=1.3.4',
     'setuptools~=58.5.3',
     'networkx~=2.6.3',
     'pygraphviz~=1.7',
     'pytest~=6.2.5',
-    'scikit-learn~=1.0.1'
+    'scikit-learn~=1.0.1',
+    'matplotlib~=3.5.0',
+    'econml~=0.12.0',
+    'statsmodels~=0.13.1'
 ]
 
 setup(
