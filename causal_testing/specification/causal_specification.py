0bdc9dcf6401541fb0484ac6b14e8f3f26e33c67
Michael <jmafoster1@gmail.com>
2021-12-08 10:36:18 +0000

    Removed comments

--- causal_testing/specification/causal_specification.py
+++ causal_testing/specification/causal_specification.py
@@ -6,8 +6,6 @@ from causal_testing.specification.scenario import Scenario
 from typing import Union
 import logging
 
-# from causal_testing.specification.scenario import Scenario
-
 Node = Union[str, int]  # Node type hint: A node is a string or an int
 logger = logging.getLogger(__name__)
 
@@ -19,25 +17,3 @@ class CausalSpecification(ABC):
 
     def __str__(self):
         return f"Scenario: {self.scenario}\nCausal DAG:\n{self.causal_dag}"
-
-
-# def main():
-#     from scipy.stats import normal
-#     from causal_testing.specification.variable import Variable
-#
-#     Vaccine = Variable("Vaccine", str, rv_discrete(values=(["Phizer"], [1])))
-#     Age = Variable("Age", float, normal(40, 10))
-#
-#     scenario = Scenario({Vaccine, Age})
-#
-#     causal_dag = CausalDAG()
-#     causal_dag.add_edge("Vaccine", "Cumulative Infections")
-#     causal_dag.add_edge("Age", "Vaccine")
-#     causal_dag.add_edge("Age", "Cumulative Infections")
-#
-#     causal_specification = CausalSpecification(scenario, causal_dag)
-#     print(causal_specification)
-#
-#
-# if __name__ == "__main__":
-#     main()
