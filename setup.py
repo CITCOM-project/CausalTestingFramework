afa8a81c5b4338cf2623ba72299722b693ef8750
Michael <jmafoster1@gmail.com>
2021-12-08 10:34:53 +0000

    Hopefully fixed setup.py after bad merge

--- setup.py
+++ setup.py
@@ -1,8 +1,21 @@
 from setuptools import setup, find_packages
 
+requirements = [
+    "numpy~=1.20",
+    "pandas~=1.3.4",
+    "setuptools~=58.5.3",
+    "networkx~=2.6.3",
+    "pygraphviz~=1.7",
+    "pytest~=6.2.5",
+    "scikit-learn~=1.0.1",
+    "matplotlib~=3.5.0",
+    "econml~=0.12.0",
+    "statsmodels~=0.13.1",
+]
+
 setup(
-    name='causal_testing_framework',
-    version='0.0.1',
+    name="causal_testing_framework",
+    version="0.0.1",
     install_requires=requirements,
-    packages=find_packages()
+    packages=find_packages(),
 )
