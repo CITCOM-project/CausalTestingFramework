2347bb51259aecedfb0c2726a2304428d820ec56
cwild-UoS <93984046+cwild-UoS@users.noreply.github.com>
2022-06-15 12:57:06 +0100

    populate and structure json abc class

--- causal_testing/json_front/json_class.py
+++ causal_testing/json_front/json_class.py
@@ -1,4 +1,152 @@
-from abc import ABC
+from pathlib import Path
 
-class frontend(ABC):
-    pass
+import json
+import pandas as pd
+import scipy
+from fitter import Fitter, get_common_distributions
+
+from causal_testing.specification.variable import Input, Output, Meta
+from causal_testing.specification.scenario import Scenario
+from causal_testing.specification.causal_dag import CausalDAG
+from causal_testing.specification.causal_specification import CausalSpecification
+from causal_testing.generation.abstract_causal_test_case import AbstractCausalTestCase
+from causal_testing.data_collection.data_collector import ObservationalDataCollector
+from causal_testing.testing.causal_test_engine import CausalTestEngine
+
+class FrontEnd(ABC):
+    def __init__(self):
+        self.json_path = None
+        self.dag_path = None
+        self.data_path = None
+        self.inputs = None
+        self.outputs = None
+        self.metas = None
+        self.data = None
+        self.test_plan = None
+        self.modelling_scenario = None
+        self.causal_specification = None
+
+    def set_paths(self, directory_path: Path):
+        """
+            Takes a path of the directory containing all scenario specific files and creates individual paths for each file
+            :param directory_path: pathlib.Path pointing towards directory containing all scenario specific user code and files
+            :returns:
+                - json_path -  path to causal_tests.json
+                - dag_path -  path to dag.dot
+                - data_path - path to scenario data, expected in data.csv
+            """
+        self.json_path = directory_path.joinpath("causal_tests.json")
+        self.dag_path = directory_path.joinpath("dag.dot")
+        self.data_path = directory_path.joinpath("data.csv")
+
+    def json_parse(self, distributions, populates):
+        """Parse a JSON input file into inputs, outputs, metas and a test plan
+            :param distributions: dictionary of user defined scipy distributions
+            :param populates: dictionary of user defined populate functions
+        """
+        with open(self.json_path) as f:
+            self.test_plan = json.load(f)
+
+        type_dict = {"float": float, "str": str, "int": int}
+
+        self.inputs = [Input(i['name'], type_dict[i['type']], distributions[i['distribution']]) for i in
+                       self.test_plan['inputs']]
+        self.outputs = [Output(i['name'], type_dict[i['type']]) for i in self.test_plan['outputs']]
+        self.metas = [Meta(i['name'], type_dict[i['type']], populates(i['populate'])) for i in
+                      self.test_plan['metas']] if "metas" in self.test_plan else []
+
+        self.data = pd.read_csv(self.data_path)
+
+    def populate_metas(self):
+        """
+        Populate data with meta-variable values and add distributions to Causal Testing Framework Variables
+        """
+
+        for meta in self.metas:
+            meta.populate(self.data)
+
+        for var in self.metas + self.outputs:
+            f = Fitter(self.data[var.name], distributions=get_common_distributions())
+            f.fit()
+            (dist, params) = list(f.get_best(method="sumsquare_error").items())[0]
+            var.distribution = getattr(scipy.stats, dist)(**params)
+            print(var.name, f"{dist}({params})")
+
+    def execute_tests(self, effects, mutates, estimators, f_flag):
+        executed_tests = 0
+        failures = 0
+        for test in self.test_plan['tests']:
+            if "skip" in test and test['skip']:
+                continue
+
+            abstract_test = AbstractCausalTestCase(
+                scenario=self.modelling_scenario,
+                intervention_constraints=[mutates[v](k) for k, v in test['mutations'].items()],
+                treatment_variables={self.modelling_scenario.variables[v] for v in test['mutations']},
+                expected_causal_effect={self.modelling_scenario.variables[variable]: effects[effect] for
+                                        variable, effect
+                                        in
+                                        test["expectedEffect"].items()},
+                effect_modifiers={self.modelling_scenario.variables[v] for v in
+                                  test['effect_modifiers']} if "effect_modifiers" in test else {},
+                estimate_type=test['estimate_type']
+            )
+
+            concrete_tests, dummy = abstract_test.generate_concrete_tests(5, 0.05)
+            print(abstract_test)
+            print([(v.name, v.distribution) for v in abstract_test.treatment_variables])
+            print(len(concrete_tests))
+            for concrete_test in concrete_tests:
+                executed_tests += 1
+                failed = self.execute_test_case(concrete_test, estimators[test['estimator']], self.modelling_scenario,
+                                                self.data_path,
+                                                self.causal_specification, f_flag)
+                if failed:
+                    failures += 1
+
+        print(f"{failures}/{executed_tests} failed")
+
+    @staticmethod
+    def execute_test_case(causal_test_case, estimator, modelling_scenario, data_path: Path, causal_specification,
+                          f_flag: bool) -> bool():
+        failed = False
+        data_collector = ObservationalDataCollector(modelling_scenario, data_path)
+        causal_test_engine = CausalTestEngine(causal_test_case, causal_specification, data_collector)
+        minimal_adjustment_set = causal_test_engine.load_data(index_col=0)
+        treatment_vars = list(causal_test_case.treatment_input_configuration)
+        minimal_adjustment_set = minimal_adjustment_set - {v.name for v in treatment_vars}
+        estimation_model = estimator((list(treatment_vars)[0].name,),
+                                     [causal_test_case.treatment_input_configuration[v] for v in treatment_vars][0],
+                                     [causal_test_case.control_input_configuration[v] for v in treatment_vars][0],
+                                     minimal_adjustment_set,
+                                     (list(causal_test_case.outcome_variables)[0].name,),
+                                     causal_test_engine.scenario_execution_data_df,
+                                     effect_modifiers=causal_test_case.effect_modifier_configuration
+                                     )
+        if "intensity" in [v.name for v in treatment_vars] and hasattr(estimation_model, "add_squared_term_to_df"):
+            estimation_model.add_squared_term_to_df("intensity")
+        if isinstance(estimation_model, Estimator):
+            estimation_model.add_product_term_to_df("width", "intensity")
+            estimation_model.add_product_term_to_df("height", "intensity")
+        causal_test_result = causal_test_engine.execute_test(estimation_model,
+                                                             estimate_type=causal_test_case.estimate_type)
+        test_passes = causal_test_case.expected_causal_effect.apply(causal_test_result)
+
+        result_string = str()
+        if causal_test_result.ci_low() and causal_test_result.ci_high():
+            result_string = f"{causal_test_result.ci_low()} < {causal_test_result.ate} <  {causal_test_result.ci_high()}"
+        else:
+            result_string = causal_test_result.ate
+        if f_flag:
+            assert test_passes, f"{causal_test_case}\n    FAILED - expected {causal_test_case.expected_causal_effect}, " \
+                                f"got {result_string}"
+        if not test_passes:
+            failed = True
+            print(f"    FAILED - expected {causal_test_case.expected_causal_effect}, got {causal_test_result.ate}")
+        return failed
+
+    def setup_scenario(self):
+        self.modelling_scenario = Scenario(self.inputs + self.outputs + self.metas, None)
+        self.modelling_scenario.setup_treatment_variables()
+        self.causal_specification = CausalSpecification(scenario=self.modelling_scenario,
+                                                        causal_dag=CausalDAG(self.dag_path))
