84f43a4871cec35db5bba95f99857c371b9e20a1
cwild-UoS <93984046+cwild-UoS@users.noreply.github.com>
2022-07-21 13:25:41 +0100

    Remove unused endpoints

--- setup.py
+++ setup.py
@@ -22,10 +22,5 @@ setup(
     name="causal_testing_framework",
     version="0.0.1",
     install_requires=requirements,
-    packages=find_packages(),
-    entry_points={
-        'console_scripts': [
-            'run_json = causal_testing.json_frontend:main'
-        ]
-    }
+    packages=find_packages()
 )
