6b3d6ad75040d142e2f79ef9b4d380f38c844038
cwild-UoS <93984046+cwild-UoS@users.noreply.github.com>
2022-08-02 12:47:27 +0100

    add json test class

---
+++ tests/json_front_tests/test_json_class.py
@@ -0,0 +1,39 @@
+import unittest
+from pathlib import Path
+import os
+from tests.test_helpers import create_temp_dir_if_non_existent, remove_temp_dir_if_existent
+from causal_testing.json_front.json_class import JsonUtility
+
+
+class TestJsonClass(unittest.TestCase):
+    """ Test the CausalTestEngine workflow using observational data.
+
+    The causal test engine (CTE) is the main workflow for the causal testing framework. The CTE takes a causal test case
+    and a causal specification and computes the causal effect of the intervention on the outcome of interest.
+    """
+
+    def setUp(self) -> None:
+        temp_dir_path = create_temp_dir_if_non_existent()
+        json_file_name = "tests.json"
+        dag_file_name = "dag.dot"
+        data_file_name = "data.csv"
+        self.json_path = os.path.join(temp_dir_path, json_file_name)
+        self.dag_path = os.path.join(temp_dir_path, json_file_name)
+        self.data_path = os.path.join(temp_dir_path, json_file_name)
+        self.json_class = JsonUtility("logs.log")
+        self.input_dict_list = [{"name": "test_input", "type": float, "distribution": "uniform"}]
+        self.output_dict_list = [{"name": "test_output", "type": float}]
+        self.meta_dict_list = [{"name": "test_meta", "type": float, "populate": "populate_num_lines_unit"}]
+
+    def test_set_paths(self):
+        self.json_class.set_path(self.json_path, self.dag_path, self.data_path)
+        self.assertEqual(self.json_class.json_path, Path(self.json_path))
+        self.assertEqual(self.json_class.dag_path, Path(self.dag_path))
+        self.assertEqual(self.json_class.data_path, Path(self.data_path))
+
+    def test_set_inputs:
+
+
+
+    def tearDown(self) -> None:
+        remove_temp_dir_if_existent()
