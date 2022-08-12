36b8c8a56f95a3ccd8eb1953d5b1823b0b770014
cwild-UoS <93984046+cwild-UoS@users.noreply.github.com>
2022-08-12 16:21:51 +0100

    fix test cases

--- tests/json_front_tests/test_json_class.py
+++ tests/json_front_tests/test_json_class.py
@@ -25,7 +25,8 @@ class TestJsonClass(unittest.TestCase):
         self.dag_path = os.path.join(temp_dir_path, json_file_name)
         self.data_path = os.path.join(temp_dir_path, json_file_name)
         self.json_class = JsonUtility("logs.log")
-        self.input_dict_list = [{"name": "test_input", "type": float, "distribution": scipy.stats.uniform(0, 1)}]
+        self.example_distribution = scipy.stats.uniform(0, 10)
+        self.input_dict_list = [{"name": "test_input", "type": float, "distribution": self.example_distribution}]
         self.output_dict_list = [{"name": "test_output", "type": float}]
         self.meta_dict_list = [{"name": "test_meta", "type": float, "populate": populate_example}]
         self.json_class.set_variables(self.input_dict_list, self.output_dict_list, self.meta_dict_list)
@@ -37,16 +38,20 @@ class TestJsonClass(unittest.TestCase):
         self.assertEqual(self.json_class.data_path, Path(self.data_path))
 
     def test_set_inputs(self):
-        ctf_input = Input("test_input", float, scipy.stats.uniform(0, 1))
-        self.assertEqual(self.json_class.inputs, ctf_input)
+        ctf_input = [Input("test_input", float, self.example_distribution)]
+        self.assertEqual(ctf_input[0].name, self.json_class.inputs[0].name)
+        self.assertEqual(ctf_input[0].datatype, self.json_class.inputs[0].datatype)
+        self.assertEqual(ctf_input[0].distribution, self.json_class.inputs[0].distribution)
 
     def test_set_outputs(self):
-        ctf_output = Output("test_output", float)
-        self.assertEqual(self.json_class.outputs, ctf_output)
+        ctf_output = [Output("test_output", float)]
+        self.assertEqual(ctf_output[0].name, self.json_class.outputs[0].name)
+        self.assertEqual(ctf_output[0].datatype, self.json_class.outputs[0].datatype)
 
     def test_set_metas(self):
-        ctf_meta = Input("test_meta", float, populate_example)
-        self.assertEqual(self.json_class.metas, ctf_meta)
+        ctf_meta = [Meta("test_meta", float, populate_example)]
+        self.assertEqual(ctf_meta[0].name, self.json_class.metas[0].name)
+        self.assertEqual(ctf_meta[0].datatype, self.json_class.metas[0].datatype)
 
     def tearDown(self) -> None:
         remove_temp_dir_if_existent()
