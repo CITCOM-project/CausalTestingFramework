ba9e2019cb4acbb59318707259db8af954e18f45
Robert (Bob) Turner <r.d.turner@sheffield.ac.uk>
2022-08-12 15:13:18 +0100

    reduce, organise dependencies

--- setup.py
+++ setup.py
@@ -1,20 +1,17 @@
 from setuptools import setup, find_packages
 
 requirements = [
-    "numpy~=1.20",
-    "pandas~=1.3.4",
-    "setuptools~=58.5.3",
-    "networkx~=2.6.3",
-    "pygraphviz~=1.7",
-    "scikit-learn~=1.0.1",
-    "matplotlib~=3.5.0",
     "econml~=0.12.0",
-    "statsmodels~=0.13.1",
-    "z3-solver~=4.8.13.0",
-    "lhsmdu",
-    "tabulate",
-    "scipy~=1.7.2",
-    "fitter~=1.4",
+    "fitter~=1.4.0",
+    "lhsmdu~=1.1",
+    "networkx~=2.6.3",
+    "numpy~=1.22.4",
+    "pandas~=1.3.5",
+    "scikit_learn~=1.1.2",
+    "scipy~=1.7.3",
+    "statsmodels~=0.13.2",
+    "tabulate~=0.8.10",
+    "z3_solver~=4.8.13.0",
 ]
 
 # Additional dependencies for development
