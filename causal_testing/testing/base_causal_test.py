94567da7bbaa6243787cce52541771402640e0fd
cwild-UoS <c.wild@sheffield.ac.uk>
2022-11-18 13:40:15 +0000

    update base_causal_test.py to use Variables

--- causal_testing/testing/base_causal_test.py
+++ causal_testing/testing/base_causal_test.py
@@ -1,11 +1,12 @@
 from dataclasses import dataclass
+from causal_testing.specification.variable import Variable
 
 @dataclass
 class BaseCausalTest:
     """
     A base causal test case represents the relationship of an edge on a causal DAG.
     """
-    treatment_variable: str
-    outcome_variable: str
+    treatment_variable: Variable
+    outcome_variable: Variable
     effect: str = 'total'
 
