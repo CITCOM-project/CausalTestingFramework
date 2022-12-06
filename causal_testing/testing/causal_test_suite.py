75c297267966f33489ebbd9260817ade3c8265dd
cwild-UoS <93984046+cwild-UoS@users.noreply.github.com>
2022-12-06 10:28:33 +0000

    add causal_test_suite.py class

---
+++ causal_testing/testing/causal_test_suite.py
@@ -0,0 +1,21 @@
+class CausalTestSuite:
+    """
+    A CausalTestSuite is a class holding a dictionary, where the keys are base_test_cases representing the treatment
+    and outcome Variables, and the values are test objects. Test Objects hold a causal_test_case_list which is a
+    list of causal_test_cases which provide control and treatment values, and an iterator of Estimator Class References
+
+    This structure can be fed into the CausalTestEngines execute_test_suite function which will iterate over all the
+    base_test_case's and execute each causal_test_case with each iterator.
+    """
+
+    def __init__(
+            self,
+    ):
+        self.test_suite = {}
+
+    def add_test_object(self, base_test_case, causal_test_case_list, estimators):
+        test_object = {'tests': causal_test_case_list, 'estimators': list(estimators)}
+        self.test_suite['base_test_case'] = test_object
+
+    def get_single_test_object(self, base_test_case):
+        return self.test_suite[base_test_case]
