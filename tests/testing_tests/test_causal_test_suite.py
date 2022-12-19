2cdb50f849473b0d00fb960703b7dd058fd96ae4
cwild-UoS <93984046+cwild-UoS@users.noreply.github.com>
2022-12-19 16:01:33 +0000

    Docstrings and comments

--- tests/testing_tests/test_causal_test_suite.py
+++ tests/testing_tests/test_causal_test_suite.py
@@ -16,12 +16,10 @@ from causal_testing.specification.causal_dag import CausalDAG
 
 
 class TestCausalTestSuite(unittest.TestCase):
-    """
-    
-    """
+    """Test the Test Suite object and it's implementation in the test engine using dummy data."""
 
     def setUp(self) -> None:
-        self.test_suite = CausalTestSuite()
+        # 1. Create dummy Scenario and BaseTestCase
         A = Input("A", float)
         self.A = A
         C = Output("C", float)
@@ -29,28 +27,15 @@ class TestCausalTestSuite(unittest.TestCase):
         D = Output("D", float)
         self.D = D
         self.base_test_case = BaseTestCase(A, C)
-        self.expected_causal_effect = ExactValue(4)
-        test_list = [CausalTestCase(self.base_test_case,
-                                    self.expected_causal_effect,
-                                    0,
-                                    1, ),
-                     CausalTestCase(self.base_test_case,
-                                    self.expected_causal_effect,
-                                    0,
-                                    2)]
-        self.estimators = [LinearRegressionEstimator]
-        self.test_suite.add_test_object(base_test_case=self.base_test_case,
-                                        causal_test_case_list=test_list,
-                                        estimators=self.estimators)
+        self.scenario = Scenario({A, C, D})
 
+        # 2. Create DAG and dummy data and write to csvs
         temp_dir_path = create_temp_dir_if_non_existent()
         dag_dot_path = os.path.join(temp_dir_path, "dag.dot")
         dag_dot = """digraph G { A -> C; D -> A; D -> C}"""
         f = open(dag_dot_path, "w")
         f.write(dag_dot)
         f.close()
-        self.causal_dag = CausalDAG(dag_dot_path)
-        self.scenario = Scenario({A, C, D})
 
         np.random.seed(1)
         df = pd.DataFrame({"D": list(np.random.normal(60, 10, 1000))})  # D = exogenous
@@ -59,31 +44,49 @@ class TestCausalTestSuite(unittest.TestCase):
         self.observational_data_csv_path = os.path.join(temp_dir_path, "observational_data.csv")
         df.to_csv(self.observational_data_csv_path, index=False)
 
+        self.causal_dag = CausalDAG(dag_dot_path)
+
+        # 3. Specify data structures required for test suite
+        self.expected_causal_effect = ExactValue(4)
+        test_list = [
+            CausalTestCase(
+                self.base_test_case,
+                self.expected_causal_effect,
+                0,
+                1,
+            ),
+            CausalTestCase(self.base_test_case, self.expected_causal_effect, 0, 2),
+        ]
+        self.estimators = [LinearRegressionEstimator]
+
+        # 3. Create test_suite and add a test
+        self.test_suite = CausalTestSuite()
+        self.test_suite.add_test_object(
+            base_test_case=self.base_test_case, causal_test_case_list=test_list, estimators=self.estimators
+        )
+
     def test_adding_test_object(self):
+        "test an object can be added to the test_suite using the add_test_object function"
         test_suite = CausalTestSuite()
-        test_list = [CausalTestCase(self.base_test_case,
-                                    self.expected_causal_effect,
-                                    0,
-                                    1)]
+        test_list = [CausalTestCase(self.base_test_case, self.expected_causal_effect, 0, 1)]
         estimators = [LinearRegressionEstimator]
-        test_suite.add_test_object(base_test_case=self.base_test_case,
-                                   causal_test_case_list=test_list,
-                                   estimators=estimators)
+        test_suite.add_test_object(
+            base_test_case=self.base_test_case, causal_test_case_list=test_list, estimators=estimators
+        )
         manual_test_object = {
-            self.base_test_case: {"tests": test_list, "estimators": estimators, "estimate_type": "ate"}}
+            self.base_test_case: {"tests": test_list, "estimators": estimators, "estimate_type": "ate"}
+        }
         self.assertEqual(test_suite, manual_test_object)
 
     def test_return_single_test_object(self):
+        """Test that a single test case can be returned from the test_suite"""
         base_test_case = BaseTestCase(self.A, self.D)
 
-        test_list = [CausalTestCase(self.base_test_case,
-                                    self.expected_causal_effect,
-                                    0,
-                                    1)]
+        test_list = [CausalTestCase(self.base_test_case, self.expected_causal_effect, 0, 1)]
         estimators = [LinearRegressionEstimator]
-        self.test_suite.add_test_object(base_test_case=base_test_case,
-                                        causal_test_case_list=test_list,
-                                        estimators=estimators)
+        self.test_suite.add_test_object(
+            base_test_case=base_test_case, causal_test_case_list=test_list, estimators=estimators
+        )
 
         manual_test_case = {"tests": test_list, "estimators": estimators, "estimate_type": "ate"}
 
@@ -97,27 +100,31 @@ class TestCausalTestSuite(unittest.TestCase):
 
         causal_test_results = causal_test_engine.execute_test_suite(test_suite=self.test_suite)
         causal_test_case_result = causal_test_results[self.base_test_case]
-        self.assertAlmostEqual(causal_test_case_result['LinearRegressionEstimator'][0].ate, 4, delta=1e-10)
+        self.assertAlmostEqual(causal_test_case_result["LinearRegressionEstimator"][0].ate, 4, delta=1e-10)
 
     def test_execute_test_suite_multiple_estimators(self):
+        """Check that executing a test suite with multiple estimators returns correct results for the dummy data
+        for each estimator
+        """
         estimators = [LinearRegressionEstimator, CausalForestEstimator]
         test_suite_2_estimators = CausalTestSuite()
-        test_list = [CausalTestCase(self.base_test_case,
-                                    self.expected_causal_effect,
-                                    0,
-                                    1)]
-        test_suite_2_estimators.add_test_object(base_test_case=self.base_test_case,
-                                                causal_test_case_list=test_list,
-                                                estimators=estimators)
+        test_list = [CausalTestCase(self.base_test_case, self.expected_causal_effect, 0, 1)]
+        test_suite_2_estimators.add_test_object(
+            base_test_case=self.base_test_case, causal_test_case_list=test_list, estimators=estimators
+        )
         causal_test_engine = self.create_causal_test_engine()
         causal_test_results = causal_test_engine.execute_test_suite(test_suite=test_suite_2_estimators)
         causal_test_case_result = causal_test_results[self.base_test_case]
-        linear_regression_result = causal_test_case_result['LinearRegressionEstimator'][0]
-        causal_forrest_result = causal_test_case_result['CausalForestEstimator'][0]
+        linear_regression_result = causal_test_case_result["LinearRegressionEstimator"][0]
+        causal_forrest_result = causal_test_case_result["CausalForestEstimator"][0]
         self.assertAlmostEqual(linear_regression_result.ate, 4, delta=1e-1)
         self.assertAlmostEqual(causal_forrest_result.ate, 4, delta=1e-1)
 
     def create_causal_test_engine(self):
+        """
+        Creating test engine is relatively computationally complex, this function allows for it to
+        easily be made in only the tests that require it.
+        """
         causal_specification = CausalSpecification(self.scenario, self.causal_dag)
 
         data_collector = ObservationalDataCollector(self.scenario, self.observational_data_csv_path)
