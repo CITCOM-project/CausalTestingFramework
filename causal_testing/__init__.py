9206041218350bc347ec595727b14d48120c141b
cwild-UoS <93984046+cwild-UoS@users.noreply.github.com>
2023-03-09 13:51:49 +0000

    Add module docstring

--- causal_testing/__init__.py
+++ causal_testing/__init__.py
@@ -1,3 +1,13 @@
+"""
+This is the CausalTestingFramework Module
+It contains 5 subpackages:
+data_collection
+generation
+json_front
+specification
+testing
+"""
+
 import logging
 
 logger = logging.getLogger(__name__)
