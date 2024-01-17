743b130f6f06f06ef23d091cf02e3f0df3d5dfc2
f-allian <allianfarhad@gmail.com>
2024-01-17 13:40:40 +0000

    Fix: linting and added __init__.py to surrogate

--- causal_testing/surrogate/surrogate_search_algorithms.py
+++ causal_testing/surrogate/surrogate_search_algorithms.py
@@ -1,5 +1,7 @@
 """Module containing implementation of search algorithm for surrogate search """
 # pylint: disable=cell-var-from-loop
+# pylint: disable=too-few-public-methods
+# pylint: disable=too-many-locals
 # Fitness functions are required to be iteratively defined, including all variables within.
 
 from operator import itemgetter
