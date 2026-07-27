052fd7f427808bdd821f38b1bef8c5c0d7b2346c
Michael Foster <m.foster@sheffield.ac.uk>
2026-07-27 11:57:35 +0100

    Added test for adequacy to dict with results

--- tests/testing_tests/test_causal_test_adequacy.py
+++ tests/testing_tests/test_causal_test_adequacy.py
@@ -127,6 +127,89 @@ class TestCausalTestAdequacy(unittest.TestCase):
         self.assertEqual(adequacy_metric.passing, 32, f"Expected passing 32 not {adequacy_metric.passing}")
         self.assertEqual(adequacy_metric.successful, 100, f"Expected successful 100 not {adequacy_metric.successful}")
 
+    def test_to_dict(self):
+        estimator = LinearRegressionEstimator(
+            treatment_variable="test_input", outcome_variable="test_output", adjustment_set=set()
+        )
+        causal_test_case = CausalTestCase(
+            expected_causal_effect=NoEffect(atol=1e-10),
+            effect_measure="coefficient",
+            estimator=estimator,
+        )
+        adequacy_metric = causal_test_case.measure_adequacy(self.df, bootstrap_size=10)
+
+        self.assertEqual(
+            adequacy_metric.to_dict(include_results=True),
+            {
+                "kurtosis": {"test_input": 0.0},
+                "passing": 10,
+                "successful": 10,
+                "results": {
+                    "effect_estimate": {
+                        0: -2.220446049250313e-16,
+                        1: -1.1102230246251565e-16,
+                        2: 7.632783294297951e-17,
+                        3: 5.551115123125783e-17,
+                        4: 6.938893903907228e-17,
+                        5: 5.551115123125783e-17,
+                        6: 4.163336342344337e-17,
+                        7: -1.6653345369377348e-16,
+                        8: 3.469446951953614e-17,
+                        9: -1.3877787807814457e-16,
+                    },
+                    "ci_low": {
+                        0: -7.771553129157294e-16,
+                        1: -2.279269223868238e-16,
+                        2: -1.8906584687236155e-16,
+                        3: -1.57411847966069e-16,
+                        4: -3.27696708597398e-17,
+                        5: 2.3327440223904727e-17,
+                        6: -6.643195454332691e-19,
+                        7: -3.158788196765981e-16,
+                        8: -1.878128765814627e-16,
+                        9: -3.348328858540416e-16,
+                    },
+                    "ci_high": {
+                        0: 3.3306610306566683e-16,
+                        1: 5.8823174617924694e-18,
+                        2: 3.4172151275832057e-16,
+                        3: 2.6843415042858463e-16,
+                        4: 1.7154754893788438e-16,
+                        5: 8.769486223861093e-17,
+                        6: 8.393104639232001e-17,
+                        7: -1.718808771094884e-17,
+                        8: 2.57201815620535e-16,
+                        9: 5.727712969775247e-17,
+                    },
+                    "test_index": {0: 0, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9},
+                    "passed": {
+                        0: True,
+                        1: True,
+                        2: True,
+                        3: True,
+                        4: True,
+                        5: True,
+                        6: True,
+                        7: True,
+                        8: True,
+                        9: True,
+                    },
+                    "var": {
+                        0: "test_input",
+                        1: "test_input",
+                        2: "test_input",
+                        3: "test_input",
+                        4: "test_input",
+                        5: "test_input",
+                        6: "test_input",
+                        7: "test_input",
+                        8: "test_input",
+                        9: "test_input",
+                    },
+                },
+            },
+        )
+
     def test_dag_adequacy_dependent(self):
         causal_test_case = CausalTestCase(
             estimator=LinearRegressionEstimator(
